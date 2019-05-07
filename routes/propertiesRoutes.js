7f41ae1ebc744efc9354dd4db6f85c3a18c4fce7
alejandrok93 <alejandrok93@gmail.com>
2019-05-07 13:58:15 -0700

    Added Properties model and routes files

---
+++ routes/propertiesRoutes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+
+const propertiesModel = require('../models/propertiesModel');
+const responseStatus = require('../config/responseStatusConfig');
