4100b35ea9e9e9a09d5f714d2d56c3ca39140f61
iAmAdamReid <adam@adamreid.me>
2019-05-08 12:22:41 -0700

    test for json response

--- index.spec.js
+++ index.spec.js
@@ -3,11 +3,19 @@ const server = require('./server.js');
 const request = require('supertest');
 
 describe('server', () => {
-    describe('GET /users', () => {
+    describe('GET /api/users', () => {
         it('should return a status of 200 (OK)', async () => {
             const response = await request(server).get('/api/users');
 
             expect(response.status).toBe(200);
         })
+
+        it('should return JSON', async () => {
+            const response = await request(server).get('/api/users');
+
+            expect(response.type).toBe('application/json');
+        })
+
+        
     })
 })
