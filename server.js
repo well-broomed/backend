38c39823aa77118e1d219106cda8c375edef0bf7
Max <maxkajiwara@gmail.com>
2019-05-13 12:28:21 -0700

    Update server.js with new routes

--- server.js
+++ server.js
@@ -20,15 +20,15 @@ server.get('/', (req, res) => {
 // Routes
 const userRoutes = require('./routes/userRoutes');
 const inviteRoutes = require('./routes/inviteRoutes');
-// const propertyRoutes = require('./routes/propertyRoutes');
-// const guestRoutes = require('./routes/guestRoutes');
-// const taskRoutes = require('./routes/taskRoutes');
+const propertyRoutes = require('./routes/propertyRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+const guestRoutes = require('./routes/guestRoutes');
 
 // Endpoints
 server.use('/api/users', userRoutes);
 server.use('/api/invites', inviteRoutes);
-// server.use('/properties', propertyRoutes);
-// server.use('/guests', guestRoutes);
-// server.use('/tasks', taskRoutes);
+server.use('/properties', propertyRoutes);
+server.use('/tasks', taskRoutes);
+server.use('/guests', guestRoutes);
 
 module.exports = server;
