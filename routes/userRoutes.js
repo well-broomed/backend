417d54169abd5d643df12dc457eccb155c104355
iAmAdamReid <adam@adamreid.me>
2019-05-13 15:00:33 -0700

    fix split login for different user types

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,12 +13,14 @@ const generateToken = require('../helpers/generateToken');
 /* Check for a valid token, add the user to our db if they aren't registered, and create a partnership if provided a valid invite code */
 router.post('/login/:inviteCode*?', checkJwt, async (req, res) => {
 	const { nickname: user_name, email, picture: img_url, exp } = req.user;
-	const { role } = req.body;
+	console.log('reqbody', req.body);
+	const role = req.body.role;
 	const { inviteCode } = req.params;
 
 	// TODO: verify arguments are properly formatted and respond with errors for bad strings
 
 	try {
+		console.log('routerole', role);
 		// Find user else create a new one
 		const user =
 			(await userModel.getUserByEmail(email)) ||
