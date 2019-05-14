b1bbedfc578d09293909895c13d978bda5c5492e
Max <maxkajiwara@gmail.com>
2019-05-14 03:24:57 -0700

    Update checkForDuplicates to handle update methods

--- helpers/checkForDuplicates.js
+++ helpers/checkForDuplicates.js
@@ -1,15 +1,18 @@
-function checkForDuplicates(properties, array, identifier) {
+const checkForDuplicates = (properties, array, identifier, excluding) => {
 	const notUnique = {};
 
 	for (const [key, value] of Object.entries(properties)) {
 		array.forEach(object => {
-			if (object[key] === value) {
+			if (
+				!(excluding && object[excluding.key] == excluding.value) &&
+				object[key] == value
+			) {
 				notUnique[key] = object[identifier];
 			}
 		});
 	}
 
 	return notUnique;
-}
+};
 
 module.exports = checkForDuplicates;
