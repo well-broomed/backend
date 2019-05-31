cb41dcf24f9e33858801a53c2b70b7532752e2e9
Max <maxkajiwara@gmail.com>
2019-05-30 20:05:18 -0700

    Remove unnecessary options object

--- helpers/generateToken.js
+++ helpers/generateToken.js
@@ -3,8 +3,7 @@ const jwt = require('jsonwebtoken');
 const generateToken = ({ user_id, user_name, email, role, img_url }, exp) => {
 	return jwt.sign(
 		{ user_id, user_name, email, role, img_url, exp },
-		process.env.JWT_SECRET,
-		{}
+		process.env.JWT_SECRET
 	);
 };
 
